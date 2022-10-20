57fb363a477a8a5116aadd375815402f10e57151
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-10-20 22:09:01 +1000

    Update OnboardingController

--- simple-quiz-app/simple-quiz-app/OnboardingController.swift
+++ simple-quiz-app/simple-quiz-app/OnboardingController.swift
@@ -11,10 +11,16 @@ import UIKit
 final class OnboardingController: UIViewController {
 
     var model: OnboardingModel?
+    
+    private var onboardingView: OnboardingView? {
+        guard isViewLoaded else { return nil }
+        return view as? OnboardingView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view = OnboardingView()
         model = OnboardingModel()
 
         configureView()
